1285a22b0ec51d5c97446476f3b660ab2f70b0be
Olav Schettler <olav@schettler.net>
2021-08-20 11:08:16 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Permacrunch</title>
+    <meta name="author" content="Dr. Olav Schettler">
+    <link rel='stylesheet' href='//cdn.jsdelivr.net/npm/hack-font@3.3.0/build/web/hack-subset.css'>
+    <script src="https://hypothes.is/embed.js" async></script>
+    <style>
+    html {
+      background: url(sofa.jpg) no-repeat center top fixed;
+      -webkit-background-size: cover;
+      -moz-background-size: cover;
+      -o-background-size: cover;
+      background-size: cover;
+    }
+    body {
+      margin: 0;
+      font-family: Hack, sans-serif;
+    }
+    main {
+      font-size: 2em;
+      width: 50%;
+      text-align: center;
+      margin: 50% auto;
+      color: white;
+      text-shadow: 3px 3px 4px #777
+    }
+    a {
+      color: white;
+      text-shadow: 3px 3px 4px #777
+    }
+    footer {
+      position: fixed;
+      right: 0;
+      bottom: 0;
+      padding: 4px 10px;
+      width: 100%;
+      color: #CCC;
+      text-shadow: 3px 3px 4px #777;
+      text-align: right;
+    }
+    </style>
+  </head>
+  <body>
+    <main>
+      <h1>Permacrunch</h1>
+      <p>A community of founders who are in the game for the long run..</p>
+    </main>
+    <footer>&copy; 2020 Dr. Olav Schettler &middot; Pipinstr. 14, 53111 Bonn | <a href="mailto:info@permacrunch.com?subject=Website">Contact</a>
+    </footer>
+  </body>
+</html>
